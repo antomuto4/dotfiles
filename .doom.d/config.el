fe382ae783a56ef0f7024ddf829226920d8a5fed
Antomuto4 <josefrodruiz@tutnota.com>
2020-09-25 20:24:59 +0200

    added: .emacs.d/ .doom.d/

---
+++ .doom.d/config.el
@@ -0,0 +1,10 @@
+;; Setting the Font
+(setq doom-font (font-spec :family "Dina ttf 10px" :size 16))
+
+;; Setting the Theme
+;; (setq doom-theme 'doom-tomorrow-night)
+
+(setq doom-theme 'doom-nord)
+
+
+;; (setq doom-theme 'doom-wilmersdorf)
